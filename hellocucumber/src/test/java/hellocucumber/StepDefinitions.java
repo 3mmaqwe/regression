1e399d5631d2fffd0d194448651cc474274a0218
emma <emmawoodstock@gmail.com>
2025-04-16 14:52:55 -0500

    added example files from cucumber tutorial to here

--- hellocucumber/src/test/java/hellocucumber/StepDefinitions.java
+++ hellocucumber/src/test/java/hellocucumber/StepDefinitions.java
@@ -4,7 +4,15 @@ import io.cucumber.java.en.*;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+class IsItFriday {
+    static String isItFriday(String today) {
+        return "Friday".equals(today) ? "TGIF" : "Nope";
+    }
+}
+
 public class StepDefinitions {
+    private String today;
+    private String actualAnswer;
 
     @Given("an example scenario")
     public void anExampleScenario() {
@@ -18,4 +26,22 @@ public class StepDefinitions {
     public void theScenarioPasses() {
     }
 
+    @Given("today is {string}")
+    public void today_is(String today) {
+        this.today = today;
+    }
+
+    @When("I ask whether it's Friday yet")
+    public void i_ask_whether_it_s_Friday_yet() {
+        actualAnswer = IsItFriday.isItFriday(today);
+    }
+
+    @Then("I should be told {string}")
+    public void i_should_be_told(String expectedAnswer) {
+        assertThat(actualAnswer).isEqualTo(expectedAnswer);
+    }
+
+
 }
+
+
